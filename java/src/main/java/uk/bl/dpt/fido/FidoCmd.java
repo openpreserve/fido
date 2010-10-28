87cbba5aebbe141cc2a42e1a0d25dfb688e2dddb
Andrew Jackson <Andrew.Jackson@bl.uk>
2010-10-28 18:07:09 +0100

    Will now cope with an authenticating proxy.

--- java/src/main/java/uk/bl/dpt/fido/FidoCmd.java
+++ java/src/main/java/uk/bl/dpt/fido/FidoCmd.java
@@ -5,10 +5,11 @@ package uk.bl.dpt.fido;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 
 import javax.xml.bind.JAXBException;
 
-import uk.gov.nationalarchives.pronom.SigFile;
 import uk.gov.nationalarchives.pronom.SignatureFileType;
 
 /**
@@ -23,8 +24,28 @@ public class FidoCmd {
 	 * @throws FileNotFoundException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException, JAXBException {
+		// To make java.net.URL cope with an authenticating proxy.
+		String proxyUser = System.getProperty("http.proxyUser");
+		if (proxyUser != null) {
+            Authenticator.setDefault(
+            		new ProxyAuth( proxyUser, System.getProperty("http.proxyPassword") ) );
+		}
+		
 		SignatureFileType sigFile = SigFileUtils.getLatestSigFile().getFFSignatureFile();
 		SigFileUtils.writeSigFileToOutputStream(sigFile, new FileOutputStream("signaturefile.xml"));
 	}
+	
+	static class ProxyAuth extends Authenticator {
+	    private PasswordAuthentication auth;
+
+	    private ProxyAuth(String user, String password) {
+	        auth = new PasswordAuthentication(user, password == null ? new char[]{} : password.toCharArray());
+	    }
+
+	    protected PasswordAuthentication getPasswordAuthentication() {
+	        return auth;
+	    }
+	}
+
 
 }
