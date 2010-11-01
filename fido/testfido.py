e39d13f28ef8e324f5102fc077565104ddf6000c
AFarquha <AFarquha@XPLT200596.ad.bl.uk>
2010-11-01 15:43:23 +0000

    Updated tests to handle the '(?s)' prefix on regexes.

--- fido/testfido.py
+++ fido/testfido.py
@@ -10,32 +10,32 @@ class Test(unittest.TestCase):
     def test_convert(self):
         "convertToRegex checking BOF/EOF/VAR with all features"
         tests = [
-        (('414141', 'Little', 'Absolute from BOF', '0', ''), r'\AAAA'),
-        (('4141*42', 'Little', 'Absolute from BOF', '0', ''), r'\AAA.*B'),
-        (('4141[42:43]', 'Little', 'Absolute from BOF', '0', ''), r'\AAA[B-C]'),
-        (('4141{0-30}41', 'Little', 'Absolute from BOF', '0', ''), r'\AAA.{0,30}A'),
-        (('4142', 'Little', 'Absolute from BOF', '5', ''), r'\A.{5}AB'),
-        (('4142', 'Little', 'Absolute from BOF', '5', '20'), r'\A.{5,20}AB'),
+        (('414141', 'Little', 'Absolute from BOF', '0', ''), r'(?s)\AAAA'),
+        (('4141*42', 'Little', 'Absolute from BOF', '0', ''), r'(?s)\AAA.*B'),
+        (('4141[42:43]', 'Little', 'Absolute from BOF', '0', ''), r'(?s)\AAA[B-C]'),
+        (('4141{0-30}41', 'Little', 'Absolute from BOF', '0', ''), r'(?s)\AAA.{0,30}A'),
+        (('4142', 'Little', 'Absolute from BOF', '5', ''), r'(?s)\A.{5}AB'),
+        (('4142', 'Little', 'Absolute from BOF', '5', '20'), r'(?s)\A.{5,20}AB'),
         
-        (('414141', 'Little', 'Absolute from EOF', '0', ''), r'AAA\Z'),
-        (('4141*42', 'Little', 'Absolute from EOF', '0', ''), r'AA.*B\Z'),
-        (('4141[42:43]', 'Little', 'Absolute from EOF', '0', ''), r'AA[B-C]\Z'),
-        (('4141{0-30}41', 'Little', 'Absolute from EOF', '0', ''), r'AA.{0,30}A\Z'),
-        (('4142', 'Little', 'Absolute from EOF', '5', ''), r'AB.{5}\Z'),
-        (('4142', 'Little', 'Absolute from EOF', '5', '20'), r'AB.{5,20}\Z'),
+        (('414141', 'Little', 'Absolute from EOF', '0', ''), r'(?s)AAA\Z'),
+        (('4141*42', 'Little', 'Absolute from EOF', '0', ''), r'(?s)AA.*B\Z'),
+        (('4141[42:43]', 'Little', 'Absolute from EOF', '0', ''), r'(?s)AA[B-C]\Z'),
+        (('4141{0-30}41', 'Little', 'Absolute from EOF', '0', ''), r'(?s)AA.{0,30}A\Z'),
+        (('4142', 'Little', 'Absolute from EOF', '5', ''), r'(?s)AB.{5}\Z'),
+        (('4142', 'Little', 'Absolute from EOF', '5', '20'), r'(?s)AB.{5,20}\Z'),
 
-        (('414141', 'Little', 'Variable', '0', ''), r'AAA'),
-        (('4141*42', 'Little', 'Variable', '0', ''), r'AA.*B'),
-        (('4141[42:43]', 'Little', 'Variable', '0', ''), r'AA[B-C]'),
-        (('4141{0-30}41', 'Little', 'Variable', '0', ''), r'AA.{0,30}A'),
-        (('31[09:0B]36', 'Little', 'Variable', '', ''), '1[\\\t-\\\x0b]6'),
+        (('414141', 'Little', 'Variable', '0', ''), r'(?s)AAA'),
+        (('4141*42', 'Little', 'Variable', '0', ''), r'(?s)AA.*B'),
+        (('4141[42:43]', 'Little', 'Variable', '0', ''), r'(?s)AA[B-C]'),
+        (('4141{0-30}41', 'Little', 'Variable', '0', ''), r'(?s)AA.{0,30}A'),
+        (('31[09:0B]36', 'Little', 'Variable', '', ''), '(?s)1[\\\t-\\\x0b]6'),
         (('3132(3333|343434)35??36*{250-*}37{10-20}', 'Little', 'Variable', '', ''),
-         '12(?:33|444)5.?6.*.{250,}7.{10,20}'),
-        (('41??', 'Little', 'Variable', '0', ''), r'A.?'),
+         '(?s)12(?:33|444)5.?6.*.{250,}7.{10,20}'),
+        (('41??', 'Little', 'Variable', '0', ''), r'(?s)A.?'),
         ]
         i = 0
         for (args, expected) in tests: 
-            result = prepare.convertToRegex(*args)
+            result = prepare.convert_to_regex(*args)
             self.assertEqual(result, expected, 'Failed conversion. Item {}: Expected {!r}, got {!r}'.format(i, expected, result))
             i += 1
 
