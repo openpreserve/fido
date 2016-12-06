cb9ce394d4a5ffdb4e1220e5d0dfd2dc98a61033
Oskar Persson <oskar.persson@polken.se>
2016-12-06 09:27:28 +0100

    Fix flake8 warning

--- fido/prepare.py
+++ fido/prepare.py
@@ -344,6 +344,7 @@ def doByte(chars, i, littleendian):
         val = chr(c1 + 16 * c2)
     return (escape(val), 2)
 
+
 # \a\b\n\r\t\v
 # MdR: took out '<' and '>' out of _ordinary because they were converted to entities &lt;&gt;
 # MdR: moved '!' from _ordinary to _special because it means "NOT" in the regex world. At this time no regex in any sig has a negate set, did this to be on the safe side
